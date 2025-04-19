011bc6c29d48808e767f68f4e1a12a2cfeffce23
Nelson Hernandez <nelson.hernandez@trade.ec>
2025-04-18 22:13:07 -0400

    add tags

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -1,16 +1,16 @@
 import { TagType } from "@prisma/client";
 import { type ParseKeys } from "i18next";
 
-export const tagsTranslations: Record<TagType, ParseKeys> = {
-  [TagType.high_protein]: "tags.highProtein",
-  [TagType.high_fiber]: "tags.highFiber",
-  [TagType.low_fat]: "tags.lowFat",
-  [TagType.low_carb]: "tags.lowCarb",
-  [TagType.sugar_free]: "tags.sugarFree",
-  [TagType.organic]: "tags.organic",
-  [TagType.gluten_free]: "tags.glutenFree",
-  [TagType.lactose_free]: "tags.lactoseFree",
-  [TagType.vegetarian]: "tags.vegetarian",
-  [TagType.vegan]: "tags.vegan",
-  [TagType.keto]: "tags.keto",
+export const tagsTranslations: Record<TagType, string> = {
+  [TagType.keto]: 'tags.keto',
+  [TagType.vegan]: 'tags.vegan',
+  [TagType.vegetarian]: 'tags.vegetarian',
+  [TagType.low_carb]: 'tags.low_carb',
+  [TagType.sugar_free]: 'tags.sugar_free',
+  [TagType.low_fat]: 'tags.low_fat',
+  [TagType.high_protein]: 'tags.high_protein',
+  [TagType.high_fiber]: 'tags.high_fiber',
+  [TagType.organic]: 'tags.organic',
+  [TagType.gluten_free]: 'tags.gluten_free',
+  [TagType.lactose_free]: 'tags.lactose_free',
 };
