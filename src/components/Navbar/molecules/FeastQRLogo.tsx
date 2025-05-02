0128854c8bb0443d61f134e7dada141870d395e3
Nelson Hernandez <nelson.hernandez@trade.ec>
2025-05-02 00:39:08 -0400

    feature: changes images

--- src/components/Navbar/molecules/FeastQRLogo.tsx
+++ src/components/Navbar/molecules/FeastQRLogo.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Icon from "~/assets/icon.png";
+import logoBumenuqr from "~/assets/bu-menu-logo.png";
 
 export const FeastQRLogo = () => {
   return (
     <div className="flex items-center gap-3">
-      <Image src={Icon} alt="Icon" height={30} />
-      <span className="text-xl font-semibold">FeastQR</span>
+      <Image src={logoBumenuqr} alt="Icon" height={80} />
+      <span className="text-xl font-semibold">BuMenuQR</span>
     </div>
   );
 };
--- src/pageComponents/LandingPage/LandingPage.page.tsx
+++ src/pageComponents/LandingPage/LandingPage.page.tsx
@@ -182,7 +182,7 @@ export const LandingPage = async () => {
         </section>
         <PricingSection />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
